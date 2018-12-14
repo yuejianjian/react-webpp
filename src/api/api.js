793b8ed768608717460f408fc9d09f7b21127699
yuejianjian <929915906@qq.com>
2018-12-14 15:47:18 +0800

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,24 +1,24 @@
 // import {getHost} from '../utils';
 //111
 //首页
-export const loginApi=`https://api.izsjia.com/fixture/index/index`;
+export const loginApi=`/fixture/index/index`;
 // 服务流程
-export const Server=`https://api.izsjia.com/fixture/server/detail`;
+export const Server=`/fixture/server/detail`;
 // 装修学堂
-export const xuetanglist=`https://api.izsjia.com/fixture/article/list`;
+export const xuetanglist=`/fixture/article/list`;
 // 装修学堂详情页面
-export const xuedetail=`https://api.izsjia.com/fixture/article/detail`;
+export const xuedetail=`/fixture/article/detail`;
 // //精美图册
-export const tuchepic=`https://api.izsjia.com/fixture/picture/list`;
+export const tuchepic=`/fixture/picture/list`;
 // //案例列表
-export const anlipic=`https://api.izsjia.com/fixture/cases/list`;
+export const anlipic=`/fixture/cases/list`;
 // //装修价格早知道
-export const pricelist=`https://api.izsjia.com/fixture/quote/know`;
+export const pricelist=`/fixture/quote/know`;
 // //员工风采
-export const yuangong='https://api.izsjia.com/fixture/staff/list';
+export const yuangong='/fixture/staff/list';
 //  案例预约
-export const anliyuyue='https://api.izsjia.com/fixture/cases/quote';
+export const anliyuyue='/fixture/cases/quote';
 //套餐页面接口
-export const taocan= 'https://api.izsjia.com/fixture/suit/list';
+export const taocan= '/fixture/suit/list';
 //套餐详情页面接口
-export const taodetail= 'https://api.izsjia.com/fixture/suit/detail';
+export const taodetail= '/fixture/suit/detail';
